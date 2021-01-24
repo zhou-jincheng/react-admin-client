5288c037cb97ee2157b92d39580e5421339fa45c
zhou <zhou@itcast.cn>
2021-01-24 12:41:03 +0800

    初始化项目，引入路由，引入antd，引入base.css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+import Login from './pages/login'
+import Admin from './pages/admin'
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Admin} />
+      </Switch>
+    )
+  }
+}
---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Button} from 'antd'
+
+export default class Admin extends Component {
+  render() {
+    return (
+      <div>
+       <Button type="primary">这是antd按钮</Button>
+      </div>
+    )
+  }
+}
