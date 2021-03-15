a186a6e69ac51e449340a1fb8d386ed6eae60ede
zhou <zhou@itcast.cn>
2021-03-15 21:29:43 +0800

    完成图形图表模块

--- src/pages/charts/pie/index.jsx
+++ src/pages/charts/pie/index.jsx
@@ -1,10 +1,117 @@
 import React, { Component } from 'react'
+import {
+  Card
+} from 'antd'
+import ReactECharts from 'echarts-for-react'
 
 export default class Pie extends Component {
   render() {
+    const option1 = {
+      title: {
+          text: '某站点用户访问来源',
+          subtext: '纯属虚构',
+          left: 'center'
+      },
+      tooltip: {
+          trigger: 'item'
+      },
+      legend: {
+          orient: 'vertical',
+          left: 'left',
+      },
+      series: [
+          {
+              name: '访问来源',
+              type: 'pie',
+              radius: '50%',
+              data: [
+                  {value: 1048, name: '搜索引擎'},
+                  {value: 735, name: '直接访问'},
+                  {value: 580, name: '邮件营销'},
+                  {value: 484, name: '联盟广告'},
+                  {value: 300, name: '视频广告'}
+              ],
+              emphasis: {
+                  itemStyle: {
+                      shadowBlur: 10,
+                      shadowOffsetX: 0,
+                      shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+              }
+          }
+      ]
+    }
+    const option2 = {
+      backgroundColor: '#2c343c',
+  
+      title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+              color: '#ccc'
+          }
+      },
+  
+      tooltip: {
+          trigger: 'item'
+      },
+  
+      visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+              colorLightness: [0, 1]
+          }
+      },
+      series: [
+          {
+              name: '访问来源',
+              type: 'pie',
+              radius: '55%',
+              center: ['50%', '50%'],
+              data: [
+                  {value: 335, name: '直接访问'},
+                  {value: 310, name: '邮件营销'},
+                  {value: 274, name: '联盟广告'},
+                  {value: 235, name: '视频广告'},
+                  {value: 400, name: '搜索引擎'}
+              ].sort(function (a, b) { return a.value - b.value; }),
+              roseType: 'radius',
+              label: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+              },
+              labelLine: {
+                  lineStyle: {
+                      color: 'rgba(255, 255, 255, 0.3)'
+                  },
+                  smooth: 0.2,
+                  length: 10,
+                  length2: 20
+              },
+              itemStyle: {
+                  color: '#c23531',
+                  shadowBlur: 200,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+              },
+  
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: function (idx) {
+                  return Math.random() * 200;
+              }
+          }
+      ]
+    }
     return (
       <div>
-        Pie...
+        <Card title="饼图一">
+          <ReactECharts option={option1} />
+        </Card>
+        <Card title="饼图二">
+          <ReactECharts option={option2} />
+        </Card>
       </div>
     )
   }
